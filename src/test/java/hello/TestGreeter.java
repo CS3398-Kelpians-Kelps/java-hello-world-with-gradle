75513887c03bbef7d87c6db4cfe5d58b5eaf0e1c
Zach Goldberg <zag17@txstate.edu>
2019-10-02 16:29:42 -0500

    Added a passed test and a failed test

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -7,40 +7,39 @@ import org.junit.Test;
 public class TestGreeter {
    private Greeter g;
    @Before
-   public void setUp() throws Exception 
+   public void setUp() throws Exception
    {
       g = new Greeter();
    }
 
 
    @Test
-   public void testGreeterEmpty() 
+   public void testGreeterEmpty()
    {
       assertEquals(g.getName(),"");
       assertEquals(g.sayHello(),"Hello!");
    }
 
    @Test
-   public void testGreeter() 
+   public void testGreeter()
    {
       g.setName("World");
       assertEquals(g.getName(),"World");
       assertEquals(g.sayHello(),"Hello World!");
    }
-   
+
    @Test
-   public void newtestWMGreeterPass() {
-       g.setName("Boris");
-       assertEquals(g.getName(),"Boris");
-       assertEquals(g.sayHello(),"Hello Boris!");
+   public void newtestZAGGreeterPass() {
+       g.setName("Zach");
+       assertEquals(g.getName(),"Zach");
+       assertEquals(g.sayHello(),"Hello Zach!");
    }
-   /*
+
    @Test
-   public void newtestWMGreeterFail() {
-       g.setName("Sandvich");
-       assertEquals(g.getName(),"Boris");
-       assertEquals(g.sayHello(),"Hello Boris!");
+   public void newtestZAGGreeterFail() {
+       g.setName("Highlow");
+       assertEquals(g.getName(),"Zach");
+       assertEquals(g.sayHello(),"Hello!");
    }
-   */
-   
+
 }
